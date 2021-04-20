e4dc02199e679e6368b178b36044de878358a1eb
Thomas Karcher <t_karcher@posteo.de>
2021-04-20 10:24:35 +0200

    +loading words from file

--- Main.gd
+++ Main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var letters := []
+var matrix := []
 
 var not_so_random_letters = \
 	"MXSEKWAPAK" + \
@@ -14,36 +14,31 @@ var not_so_random_letters = \
 	"AWISREHDKS" + \
 	"POSSKIYJSX"
 
-var words := [
-	"APE",
-	"BAT",
-	"CAR",
-	"DRY",
-	"EAR",
-	"FLY",
-	"GOD",
-	"HORN",
-	"ILLNESS",
-	"JEOPARDY",
-	"KING"
-]
-
+var words := []
 var rects := []
 var hits := []
 
 func _ready():
-	letters.resize(10)
+	load_words()
+	matrix.resize(10)
 	for x in range(10):
-		letters[x] = []
-		letters[x].resize(10)
+		matrix[x] = []
+		matrix[x].resize(10)
 		for y in range (10):
 			var l = Label.new()
 			l.rect_position = Vector2(50 + x * 20, 50 + y * 20)
 			# letters[x][y] = char(65 + randi() % 26) 
-			letters[x][y] = not_so_random_letters[x + y * 10] 
-			l.text = letters[x][y]
+			matrix[x][y] = not_so_random_letters[x + y * 10] 
+			l.text = matrix[x][y]
 			add_child(l)
 	find_words()
+
+func load_words():
+	var file := File.new()
+	file.open("res://word_list.txt", File.READ)
+	while not file.eof_reached():
+		words.append(file.get_line())
+	file.close()
 
 func find_words():
 	for word_length in range (3,9):
@@ -52,8 +47,8 @@ func find_words():
 				var horizontal := ""
 				var vertical := ""
 				for pos in range (word_length):
-					horizontal += letters[x + pos][y] if x + pos < 10 else "-"
-					vertical += letters[x][y + pos] if y + pos < 10 else "-"
+					horizontal += matrix[x + pos][y] if x + pos < 10 else "-"
+					vertical += matrix[x][y + pos] if y + pos < 10 else "-"
 				if horizontal in words:
 					rects.append(Rect2(45 + x * 20, 45 + y * 20, 20 * word_length, 20))
 					hits.append({"word": horizontal, "pos": Vector2(x, y), "direction": "horizontal"})
@@ -65,4 +60,3 @@ func find_words():
 
 func _draw():
 	for r in rects: draw_rect(r, Color(1,0,0), false)
-
